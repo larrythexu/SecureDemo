770071461d76ef31a2bf53476169125a468fcb89
Lawrence <56418241+larrythexu@users.noreply.github.com>
2025-06-16 20:31:23 -0400

    project setup

---
+++ src/test/java/io/github/larrythexu/SecureDemo/SecureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.larrythexu.SecureDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
